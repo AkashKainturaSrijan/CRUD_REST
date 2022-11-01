dca1cb7a59ba4a5df171250eff79d8bcb19b5992
akash.kaintura <akash.kaintura@srijan.net>
2022-11-01 12:18:02 +0530

    add: CRUD operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,19 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProdModule } from './prod/prod.module';
+import { UserSchema } from './user.models';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://AkashKaintura:LPA3gHXaNXwFEugk@cluster0.v4umw92.mongodb.net/?retryWrites=true&w=majority',
     ),
+    MongooseModule.forFeature([
+      {
+        name: 'user',
+        schema: UserSchema,
+      },
+    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
